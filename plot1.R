e2355bea5bf117eeefa19feee73bf0e4d7f0c887
kon.anthis <kon.anthis@gmail.com>
2017-12-16 21:41:12 +0200

    last

--- plot1.R
+++ plot1.R
@@ -1,1 +1,6 @@
-hist(na.omit(as.numeric(coursedata$Global_active_power)),xlab = 'Global Active Power (kilowatts)',col = 'red')
+
+png(filename="plot1.png")
+
+hist(na.omit(as.numeric(coursedata$Global_active_power)),xlab = 'Global Active Power (kilowatts)',col = 'red',main = 'Global Active Power')
+
+dev.off()
